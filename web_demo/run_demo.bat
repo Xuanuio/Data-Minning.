d3ef5a4d3f124e911879c62649a4800ac3b104a7
Mai Hoàng Xuân <XuanMH.B22CN925@stu.ptit.edu.vn>
2026-05-04 09:48:45 +0700

    Delete web_demo directory

--- web_demo/run_demo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-if not exist model_artifacts\trained_extra_trees.pkl (
-  echo [INFO] Chua co model da train. Dang train truoc...
-  python train_model.py
-)
-streamlit run app.py
